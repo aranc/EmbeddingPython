e8d28ead54bc2f9ec0070cc37004f120af21101f
Aran Carmon <aran.carmon@gmail.com>
2022-04-03 18:34:07 +0300

    .

--- demo/main.c
+++ demo/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+
+//#include "bridge_api.h"
 
 int main(int argc, char** argv)
 {
+  char cmd[500];
+
   printf("GO GO GO\n");
+
+  while (1) {
+    printf(">>> ");
+    scanf("%s", cmd);
+    if (!strncmp(cmd, "quit", 5)) {
+        break;
+    }
+    printf("kkkkkk\n");
+  }
+  
   return 0;
 }
